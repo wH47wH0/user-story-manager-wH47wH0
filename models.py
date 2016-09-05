327ceacdc7fe0ef680f2ec8c313ba636857fd002
Miklos Csepella <csepella.miki@gmail.com>
2016-09-05 19:34:33 +0200

    minimal flask app, User Story Manager model

---
+++ models.py
@@ -0,0 +1,17 @@
+from peewee import *
+
+db = PostgresqlDatabase('miki', user='miki', password='645464')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class UserStoryManager(BaseModel):
+    story_title = CharField
+    user_story = TextField
+    acceptance_criteria = TextField
+    business_value = IntegerField
+    estimation = IntegerField
+    status = CharField
